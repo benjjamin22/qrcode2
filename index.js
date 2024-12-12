078068f1d1ac82d2cd475a4e79940eb2c1387cdc
benjamin edigbe <deeprising4ilife@gmail.com>
2024-12-12 10:01:27 -0800

    first commit

--- index.js
+++ index.js
@@ -51,7 +51,8 @@ app.post('/login', async (req, res) => {
                 res.render('result',{Name:foundUser.Aname.Name,Mname:foundUser.Aname.Mname,Surname:foundUser.Aname.Surname,NIN:foundUser.NIN,Gender:foundUser.Gender,Day:foundUser.Ddateofbirth.Day,Month:foundUser.Ddateofbirth.Month,Year:foundUser.Ddateofbirth.Year,Presentclass:foundUser.Presentclass,Bloodgroup:foundUser.Bloodgroup,State:foundUser.State,School:foundUser.School,HometownCommunity:foundUser.HometownCommunity,ParentPhoneNo:foundUser.ParentPhoneNo,ParentPhoneNo2:foundUser.ParentPhoneNo2,Picturepath:foundUser.client,Status:foundUser.Status,id:foundUser.id});
                 //res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello </h3></div><br><br><div align='center'><a href='./lohtml'>logout</a></div>`);
             } else {
-            res.send("<div align ='center'><h2>Invalid ID NO /h2></div><br><br><div align ='center'><a href='wwww.isemb.mydatabase.com.ng'>login again</a></div>");
+                res.render('ddx');
+            //res.send("<div align ='center'><h2>Invalid ID NO /h2></div><br><br><div align ='center'><a href='wwww.isemb.mydatabase.com.ng'>login again</a></div>");
             }
         //}
        
