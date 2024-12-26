2e7b15793d7c13a8877780ad63e3c35b358d2f4a
benjamin edigbe <deeprising4ilife@gmail.com>
2024-12-26 00:27:27 -0800

    first commit

--- index.js
+++ index.js
@@ -16,9 +16,11 @@ const port = process.env.PORT || 8000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const accounts = JSON.parse(fs.readFileSync('./data.json','utf-8'))
+const accounts = JSON.parse(fs.readFileSync('./data.json','utf-8'));
+const keys = JSON.parse(fs.readFileSync('./pass.json','utf-8'));
 
 const app = express();
+app.use(express.json())
 
 // setup static folder
 app.use(bodyParser.urlencoded({extended: true}));
@@ -61,6 +63,53 @@ app.post('/login', async (req, res) => {
         
     }
 });
+
+
+app.post('/Armyday', async (req, res) => {
+    try{
+        const foundUser = keys.find((data) => req.body.Password === data.pine);
+        if (foundUser) {
+                //res.render('result',{Name:foundUser.Aname.Name,Mname:foundUser.Aname.Mname,Surname:foundUser.Aname.Surname,NIN:foundUser.NIN,Gender:foundUser.Gender,Day:foundUser.Ddateofbirth.Day,Month:foundUser.Ddateofbirth.Month,Year:foundUser.Ddateofbirth.Year,Presentclass:foundUser.Presentclass,Bloodgroup:foundUser.Bloodgroup,State:foundUser.State,School:foundUser.School,HometownCommunity:foundUser.HometownCommunity,ParentPhoneNo:foundUser.ParentPhoneNo,ParentPhoneNo2:foundUser.ParentPhoneNo2,Picturepath:foundUser.client,Status:foundUser.Status,id:foundUser.id});
+                res.sendFile(path.join(__dirname, 'public','/asso/Asso.html'));
+            } else {
+                res.render('ddx');
+            //res.send("<div align ='center'><h2>Invalid ID NO /h2></div><br><br><div align ='center'><a href='wwww.isemb.mydatabase.com.ng'>login again</a></div>");
+            }
+        //}
+       
+    } catch{
+        res.send("Internal server error");
+        
+    }
+});
+
+app.get('/nams', (req, res) => {
+    const data = accounts;
+    res.json(data)
+})
+
+
+// Route to display users
+//app.post('/Armyday', (req, res) => {
+
+  //const foundUser = key.find((data) => req.body.Password === data.key || req.body.ParentPhoneNo === data.ParentPhoneNo)
+  //if (foundUser) {
+  // Read the JSON file
+  
+
+    // Parse the JSON data
+    //const users = accounts;
+
+    // Render the EJS template and pass the users data
+    //res.render("armyday", { users });
+  
+//} else {
+  //res.render('ddx');
+//res.send("<div align ='center'><h2>Invalid ID NO /h2></div><br><br><div align ='center'><a href='wwww.isemb.mydatabase.com.ng'>login again</a></div>");
+//}
+//}
+//});
+
 
 // Error handler
 app.use(notFound);
