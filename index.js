801587dc3b406c84e2fe3d4ed734632b6853a040
benjamin edigbe <deeprising4ilife@gmail.com>
2025-01-19 06:39:34 -0800

    first commit

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ app.post('/login', async (req, res) => {
         const foundUser = accounts.find((data) => req.body.Password === data.pine || req.body.ParentPhoneNo === data.ParentPhoneNo);
         if (foundUser) {
             req.session.user = foundUser.pine;
-                res.render('idslip',{Name:foundUser.Aname.Name,Mname:foundUser.Aname.Mname,Surname:foundUser.Aname.Surname,
+                res.render('result',{Name:foundUser.Aname.Name,Mname:foundUser.Aname.Mname,Surname:foundUser.Aname.Surname,
                     NIN:foundUser.NIN,Gender:foundUser.Gender,Day:foundUser.Ddateofbirth.Day,Month:foundUser.Ddateofbirth.Month,
                     Year:foundUser.Ddateofbirth.Year,Presentclass:foundUser.Presentclass,Bloodgroup:foundUser.Bloodgroup,
                     State:foundUser.State,School:foundUser.School,HometownCommunity:foundUser.HometownCommunity,
